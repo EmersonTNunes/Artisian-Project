de898f34e3a18b19350b3eca6094c4b8001affd5
Arthur <tuco1301@gmail.com>
2024-04-23 14:09:58 -0300

    função de escrever em excel

---
+++ main.py
@@ -0,0 +1,60 @@
+import openpyxl
+import pandas
+
+def overview_data():
+	print("Insira a data")
+	data = input()
+
+	print("Selecione o número do bot:\n1 - Sicoob BOT\n2- Não Cooperados\n3 - SIPAG\n4 - Coopera\n5 - Coopcerto\n 6 - Sicoobcard")
+	bot = input()
+	if bot == "1":
+		bot = "Sicoob BOT"
+	elif bot == "2":
+		bot = "Não Cooperados"
+	elif bot == "3":
+		bot = "SIPAG"
+	elif bot == "4":
+		bot = "Coopera"
+	elif bot == "5":
+		bot = "Coopcerto"
+	elif bot == "6":
+		bot = "Sicoobcard"
+	
+	print("Insira o número de Atendimentos")
+	atendimentos = input()
+
+	print("Insira o número de Atendimentos Contidos (Contained)")
+	contained = input()
+
+	print("Insira o número de Interações")
+	interacoes = input()
+
+	print("Insira o número de Interações com resposta (Covered)")
+	covered = input()
+
+	print("Insira o número de usuários")
+	usuarios = input()
+
+	list_data = [data, bot, atendimentos, contained, interacoes, covered, usuarios]
+
+	return list_data
+
+def write_on_excel_file(file_name, data):
+	# Recebe o nome do arquivo e uma lista de dados
+	
+	# Abre o arquivo
+	workbook = openpyxl.load_workbook(file_name)
+
+	# adiciona dados na tabela
+	sheet = workbook.active
+
+	# Adiciona os dados na planilha
+	sheet.append(data)
+
+	# Salva o arquivo
+	workbook.save(file_name)
+
+data = overview_data()
+write_on_excel_file('overview.xlsx', data)
+
+
